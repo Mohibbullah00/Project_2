df6a693071e61f1945238d6aa0140dda55d7710b
Mohibbullah00 <119688262+Mohibbullah00@users.noreply.github.com>
2024-03-29 05:40:35 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+document.getElementById("bars").addEventListener('click', function(){
+    document.getElementById("side").classList.add('active')
+});
+
+document.getElementById("close_bars").addEventListener('click', function(){
+    document.getElementById("side").classList.remove('active')
+});
